c3199d5277b22893a6b8764c68cf3e65a81a2c3e
Burak Abanoz <brk.abanoz@hotmail.com>
2026-03-15 02:51:49 +0300

    fix: tailwind css entegrasyonu düzeltildi

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,10 @@
-// @ts-check
-
+import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-import { defineConfig } from 'astro/config';
-
-import tailwindcss from '@tailwindcss/vite';
+import tailwind from '@astrojs/tailwind'; // BU SATIR ÇOK ÖNEMLİ
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
-  integrations: [mdx(), sitemap()],
-
-  vite: {
-    plugins: [tailwindcss()],
-  },
+  integrations: [mdx(), sitemap(), tailwind()], // BURADA TAILWIND OLMALI
 });
